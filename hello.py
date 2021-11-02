f13be5eb9457a8c90872671d3246611e28147b13
mjantson <martinjantson97@gmail.com>
2021-11-02 15:25:55 +0200

    initial commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route('/')
+
+def index():
+    first_name = "John"
+    stuff = "This is bold text"
+
+    favourite_pizza=["Pepperoni", "Cheese", "Mushrooms", 41]
+    return render_template("index.html", 
+    first_name=first_name,
+    stuff=stuff,
+    favourite_pizza=favourite_pizza)
+
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html", user_name=name)
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal sercer error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
